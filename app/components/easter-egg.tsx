84c51988215ccb3d4e4ec29ce2edb5321fc02f65
Jeong TaeSeung <mike415415@naver.com>
2025-12-02 15:18:24 +0900

    layout : 이스터에그 컴포넌트

---
+++ app/components/easter-egg.tsx
@@ -0,0 +1,10 @@
+
+export const EasterEgg_Inheader = ()=>{
+    return (
+    <div className="absolute top-0 left-0 opacity-0 hover:opacity-100 hover:z-150 transition-opacity duration-300">
+        <h1>야 도란</h1>
+        <h2>도란 도란</h2>
+        <h3>도란 도란 도란</h3>
+      </div>
+    )
+}
