6a900b889ac18ce999cddf3107ce5620d8d09b0e
Jeong TaeSeung <mike415415@naver.com>
2025-12-02 14:16:30 +0900

    layout : 메인 페이지 기본소개 레이아웃 구조 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,70 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div>
-      <div className="flex flex-col gap-2 justify-center items-center w-screen h-screen">
-          테스트입니다.
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+    <section>
+      <div className="flex min-w-[50vw] max-w-full h-screen p-4 gap-6 justify-center items-center">
+        
+        <div
+        aria-label="Profile Image and Links"
+        className = "flex flex-col">
+          <div className ="flex w-[500px] h-[500px] justify-center items-center">
+            <Image
+              className="rounded-full grayscale-100"
+              src="/image/profile.jpg"
+              alt="Profile picture"
+              width={300}
+              height={400}
+              priority
+            />
+          </div>
+            <div className = "flex flex-col items-center text-2xl gap-2">
+
+              <a className ="flex gap-2 items-center" href="https://github.com/taeseungjeong">
+              <Image
+                src="/svg-icon/github.svg"
+                alt="Signature"
+                width={24}
+                height={24}
+    
+              />
+              <span className = "underline underline-offset-4">Github</span>
+              </a>
+
+              <a className ="flex gap-2 items-center" href="https://www.notion.so/taeseungjeong/taeseungjeong-s-Portfolio-Page-6b2f3fbf3f3b4e1eaed3f5e0e4c8e4b3">
+              <Image
+                src="/svg-icon/notion.svg"
+                alt="Signature"
+                width={24}
+                height={24}
+                
+              />
+              <span className = "underline underline-offset-4">Notion</span>
+              </a>
+
+            </div>
+        </div>
+
+        <div aria-label="Introduction" className ="flex flex-col gap-20">
+          <div className ="space-y-2 whitespace-nowrap">
+            <h1 className ="text-6xl font-bold">재미를 구현하는 개발자</h1>
+            <h2 className="text-5xl">정태승입니다</h2>
+          </div>
+
+
+          <p className ="text-xl">
+            {"까불지 마세요 여러분 저는 개발자입니다."} <br />
+            {"당신들 다 큰일난거야 알아?"}<br />
+            {"저는 재미있는 것을 만드는 개발자 정태승입니다."}
+          </p>
+    
+
+        </div>
       </div>
-      <div className="flex justify-center items-center w-screen h-screen">
+      <div className="flex justify-center items-center w-full h-screen">
           테스트입니다.
       </div>
-    </div>
+    </section>
   );
 }
