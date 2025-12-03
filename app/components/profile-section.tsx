b2c5a3fbdcf0f578d3b5d91962ce8121db9daa98
Jeong TaeSeung <mike415415@naver.com>
2025-12-03 11:17:56 +0900

    feat : 프로필 섹션  컴포넌트 분리

---
+++ app/components/profile-section.tsx
@@ -0,0 +1,31 @@
+import { certificationList, educationList, rewardsList } from "../const/profileList";
+import { DateDescriptionList } from "./date-description-list";
+import FullSection from "./full-section";
+
+
+export default function ProfileSection() {
+    return(
+          <FullSection>
+                <div className="flex flex-col items-center gap-y-16 min-w-1/2">
+                  <h1 className = "text-4xl font-semibold">Profile</h1>
+        
+                  <div className = "relative grid grid-cols-2 grid-rows-2 gap-x-48 gap-y-28  place-items-center">
+                    <div className="space-y-4 opacity-0 translate-y-6">
+                      <h2 className ="text-3xl"> Education </h2>
+                      <DateDescriptionList items={educationList}/>
+                    </div>
+                    <div className="space-y-4 opacity-0 translate-y-6">
+                      <h2 className ="text-3xl flex justify-start"> Rewards </h2>
+                      <DateDescriptionList items={rewardsList}/>
+                    </div>
+                    <div className="space-y-4 opacity-0 translate-y-6 col-start-2">
+                      <h2 className ="text-3xl"> Certification </h2>
+                      <DateDescriptionList items={certificationList}/>
+                    </div>
+        
+                    <div className="absolute left-1/2 h-full w-0.5 bg-gray-500"></div>
+                  </div>
+                </div>
+              </FullSection>
+    )
+}
